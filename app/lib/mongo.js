ee13efd444560b280d0788d4664697e32e7a069c
NithinKonda <nithinkonda142@gmail.com>
2024-12-22 23:52:53 +0530

    feat : add MongoDB integration and update dependencies

---
+++ app/lib/mongo.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+
+const uri = process.env.MONGODB_URI || "";
+let client;
+let clientPromise;
+
+if (!uri) {
+  throw new Error("Please add your MongoDB URI to .env.local");
+}
+
+if (process.env.NODE_ENV === "development") {
+
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
+module.exports = clientPromise;
