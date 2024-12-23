cf1b31cef74f275519a2ac2b0b44031c843b1636
NithinKonda <nithinkonda142@gmail.com>
2024-12-23 12:11:05 +0530

    feat: add Navbar component with links to File Activity Log, Usage Log, and File Extensions

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4 shadow-md">
+      <ul className="flex justify-around">
+        <li>
+          <a href="/file-activity-log" className="hover:text-gray-400">
+            File Activity Log
+          </a>
+        </li>
+        <li>
+          <a href="/usage-log" className="hover:text-gray-400">
+            Usage Log
+          </a>
+        </li>
+        <li>
+          <a href="/file-extensions" className="hover:text-gray-400">
+            File Extensions
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
